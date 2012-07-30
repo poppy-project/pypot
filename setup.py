f267ce84e7de4a51280ed1a3657ca2943c25dc14
Pierre Rouanet <pierrerouanet@Teahupoo.local>
2012-07-30 19:08:25 +0200

    Create an empty skeletal for the dynamixel part.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyPot',
+      version='0.1.0',
+      author='Matthieu Lapeyre, Pierre Rouanet',
+      author_email='pierre.rouanet@gmail.com',
+      url='https://bitbucket.org/pierrerouanet/pypot',
+      description='Python Library for Robot Control'
+      )
---
+++ test/test_dynamixel.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestDynamixel(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_connection(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
