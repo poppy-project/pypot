ae0f5136fb5af8432c51aba5e5366182a98f21fb
Pierre Rouanet <pierre.rouanet@gmail.com>
2013-09-30 16:47:18 +0200

    Correct a bug in the request handler that prevented from using more than one argument in a call request.
    Rewrite the http server primitive which can now handle the following request:
    - GET /motor/list.json
    - GET /primitive/list.json
    - GET /motor/<name>/register.json (eq. GET /<name>/register.json)
    - GET /motor/<name>/<register> (eq. GET /<name>/<register>)
    - GET /motor/<name>/<register> (eq. GET /<name>/<register>)
    - GET /primitive/<prim_name>/call/<meth_name> (eq. GET /<prim_name>/call/<meth_name>)
    - GET /request.json

--- pypot/server/server.py
+++ pypot/server/server.py
@@ -11,14 +11,11 @@ from pypot.server.request import BaseRequestHandler
 class AbstractServer(pypot.primitive.Primitive):
     def __init__(self, robot, handler=BaseRequestHandler):
         pypot.primitive.Primitive.__init__(self, robot)
-        
+
         self.request_handler = BaseRequestHandler(self.robot)
 
 
 
 class MyJSONEncoder(json.JSONEncoder):
     def default(self, obj):
-        if hasattr(obj, 'json'):
-            return obj.json
-        else:
-            return json.JSONEncoder.default(self, obj)
+        return obj.json if hasattr(obj, 'json') else json.JSONEncoder.default(self, obj)
