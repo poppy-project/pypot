9cf77f14b3dfccf44aad93337a989f4a73b38b86
Pierre Rouanet <pierre.rouanet@gmail.com>
2013-10-01 17:09:34 +0200

    Centralize the version so when changing version you only have to edit the _version.py file. It also provides a pypot.__version__.

--- setup.py
+++ setup.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python
 
+import re
+
 from setuptools import setup, find_packages
 
 
+def version():
+  with open('pypot/_version.py') as f:
+    return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
+
+
 setup(name='pypot',
-      version='1.4.0',
+      version=version(),
       packages=find_packages(),
-      
+
       install_requires=['numpy', 'pyserial'],
-      
+
       extras_require={
         'tools': [], # Extras require: PyQt4 (not a PyPi packet)
         'doc': ['sphinx', 'sphinx-bootstrap-theme'],
       },
-      
+
       entry_points={
         'gui_scripts': [
                       'herborist = pypot.tools.herborist.herborist:main [tools]',
                       ],
       },
-      
+
       setup_requires = ['setuptools_git >= 0.3',],
-      
+
       include_package_data=True,
       exclude_package_data={'': ['README', '.gitignore']},
-      
+
       zip_safe=True,
-      
+
       author='Pierre Rouanet, Matthieu Lapeyre, Haylee Fogg',
       author_email='pierre.rouanet@gmail.com',
       description='Python Library for Robot Control',
