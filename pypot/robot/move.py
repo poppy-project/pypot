cfff093157fcea82c4446f3f2ba52d4e532c3b96
Pierre Rouanet <pierre.rouanet@gmail.com>
2014-02-17 18:55:30 +0100

    Fix a few bugs in the move recorder.

--- pypot/robot/move.py
+++ pypot/robot/move.py
@@ -4,13 +4,15 @@ from pypot.primitive import LoopPrimitive
 
 
 class Move(dict):
-    def __init__(self, framerate):
-        self['framerate'] = framerate
-        self['position'] = []
-        self._iter = iter(self['position'])
+    def __init__(self, freq):
+        dict.__init__(self, {'framerate': freq,
+                             'position': []})
 
     def add_position(self, position):
         self['position'].append(position)
+
+    def positions(self):
+        return iter(self['position'])
 
     def next(self):
         return self._iter.next()
@@ -21,33 +23,47 @@ class Move(dict):
     @classmethod
     def load(cls, file):
         d = json.load(file)
+
         m = cls(d['framerate'])
         m['position'] = d['position']
         return m
 
+
 class MoveRecorder(LoopPrimitive):
     def __init__(self, robot, freq, tracked_motors):
         LoopPrimitive.__init__(self, robot, freq)
-        self.move = Move(freq)
+        self._move = Move(freq)
 
         self.tracked_motors = map(self.get_mockup_motor, tracked_motors)
 
-
     def update(self):
         LoopPrimitive.update(self)
 
-        position = dict(zip([(m.present_position, m.name) for m in self.tracked_motors]))
-        self.move.add_position(position)
+        position = dict([(m.name, m.present_position) for m in self.tracked_motors])
+        self._move.add_position(position)
+
+    @property
+    def move(self):
+        return self._move
 
 
 class MovePlayer(LoopPrimitive):
-    def __init__(self, robot, freq, move):
-        LoopPrimitive.__init__(self, robot, freq)
+    def __init__(self, robot, move):
+        LoopPrimitive.__init__(self, robot, move['framerate'])
         self.move = move
+
+    def start(self):
+        self.positions = self.move.positions()
+        LoopPrimitive.start(self)
 
     def update(self):
         LoopPrimitive.update(self)
 
-        position = self.move.next()
-        for m, v in position.iteritems():
-            getattr(self.robot, m).goal_position = v
+        try:
+            position = self.positions.next()
+
+            for m, v in position.iteritems():
+                getattr(self.robot, m).goal_position = v
+
+        except StopIteration:
+            self.stop()
