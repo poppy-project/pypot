2862a7679139be81f835de2a31a23b98a2bb60ee
Matthieu Lapeyre <matthieu.lapeyre@inria.fr>
2014-02-17 17:52:45 +0100

    Simplify the way the move recorder works. Now it only permits a loop recording with a given frequency.

--- pypot/robot/move.py
+++ pypot/robot/move.py
@@ -1,117 +1,53 @@
-import time
-import cPickle
-import scipy.io
-import threading
+import json
 
-from itertools import cycle
-import pypot.primitive
+from pypot.primitive import LoopPrimitive
 
 
-class Move(object):
-    def __init__(self):
-        self._pos = []
-    
-    def __iter__(self):
-        return self.pos.__iter__()
-    
+class Move(dict):
+    def __init__(self, framerate):
+        self['framerate'] = framerate
+        self['position'] = []
+        self._iter = iter(self['position'])
+
+    def add_position(self, position):
+        self['position'].append(position)
+
     def next(self):
-        return self.pos.next()
-    
-    def add_position(self, dt, position):
-        self._pos.append((dt, position))
-    
-    def save(self, filename):
-        cPickle.dump(self, open(filename, 'w'))
-    
+        return self._iter.next()
+
+    def save(self, file):
+        json.dump(self, file)
+
     @classmethod
-    def load(cls, filename):
-        return cPickle.load(open(filename))
+    def load(cls, file):
+        d = json.load(file)
+        m = cls(d['framerate'])
+        m['position'] = d['position']
+        return m
 
-    @property
-    def pos(self):
-        return self._pos
+class MoveRecorder(LoopPrimitive):
+    def __init__(self, robot, freq, tracked_motors):
+        LoopPrimitive.__init__(self, robot, freq)
+        self.move = Move(freq)
 
-class MoveRecorder(pypot.primitive.Primitive):
-    def __init__(self, robot, tracked_motors, compliant=False):
-        pypot.primitive.Primitive.__init__(self, robot)
-        
-        self.tracked_motors = map(self._get_mockup_motor, tracked_motors)
-        self._move = Move()
-        self.compliant = compliant
-        
-        self.should_snap = threading.Event()
-    
-    def start(self):
-        if hasattr(self, 't0'):
-            del self.t0
-        self._move = Move()
-        pypot.primitive.Primitive.start(self)
+        self.tracked_motors = map(self.get_mockup_motor, tracked_motors)
 
-    def add_keyframe(self):
-        self.should_snap.set()
-    
-    def run(self):
-        if self.compliant:
-            for m in self.tracked_motors:
-                m.compliant = True
-                    
-        while not self.should_stop():
-            self.should_snap.wait()
-            
-            pos = dict((m.name, m.present_position) for m in self.tracked_motors)
-
-            if not hasattr(self, 't0'):
-                self.t0 = time.time()
-        
-            dt = time.time() - self.t0
-            self._move.add_position(dt, pos)
-            
-            self.should_snap.clear()
-
-        if self.compliant:
-            for m in self.tracked_motors:
-                m.compliant = False    
-    
-    @property
-    def move(self):
-        return self._move
-
-class LoopMoveRecorder(pypot.primitive.LoopPrimitive):
-    def __init__(self, robot, freq, tracked_motors, compliant=False):
-        pypot.primitive.LoopPrimitive.__init__(self, robot, freq)
-        self.move_recorder = MoveRecorder(robot, tracked_motors, compliant)
-
-    def start(self):
-        pypot.primitive.LoopPrimitive.start(self)
-        self.move_recorder.start()
 
     def update(self):
-        self.move_recorder.add_keyframe()
+        LoopPrimitive.update(self)
 
-    @property
-    def move(self):
-        return self.move_recorder.move
+        position = dict(zip([(m.present_position, m.name) for m in self.tracked_motors]))
+        self.move.add_position(position)
 
 
-class MovePlayer(pypot.primitive.Primitive):
-    def __init__(self, robot, move):
-        pypot.primitive.Primitive.__init__(self, robot)
+class MovePlayer(LoopPrimitive):
+    def __init__(self, robot, freq, move):
+        LoopPrimitive.__init__(self, robot, freq)
         self.move = move
-    
-    def play(self):
-        self.start()
-    
-    def run(self):
-        last_t = 0
-        
-        for t, pos in self.move:
-            if self.should_stop():
-                break
-            
-            if self.should_pause():
-                self.wait_to_resume()
-            
-            dt = t - last_t
-            self.robot.goto_position(pos, dt, wait=True)
-                    
-            last_t = t
+
+    def update(self):
+        LoopPrimitive.update(self)
+
+        position = self.move.next()
+        for m, v in position.iteritems():
+            getattr(self.robot, m).goal_position = v
