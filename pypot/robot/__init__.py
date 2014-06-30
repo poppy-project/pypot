32d1a32e037604bc084b912d18db562863afbbf6
Pierre Rouanet <pierre.rouanet@gmail.com>
2014-06-30 15:20:19 +0200

    Update import and installation

--- pypot/robot/__init__.py
+++ pypot/robot/__init__.py
@@ -1,2 +1,7 @@
 from .robot import Robot
 from .config import from_config, from_json
+
+try:
+    from ..vrep import from_vrep
+except ImportError:
+    pass
--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(name='pypot',
       extras_require={
           'tools': [],  # Extras require: PyQt4 (not a PyPi packet)
           'doc': ['sphinx', 'sphinx-bootstrap-theme'],
-          'server': ['bottle', 'tornado', 'zmq']
+          'server': ['bottle', 'tornado', 'zmq'],
+          'vrep': [],  # v-rep remote API (http://www.coppeliarobotics.com/helpFiles/en/remoteApiOverview.htm)
       },
 
       entry_points={
