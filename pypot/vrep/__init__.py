f5ad4661e362bbd574245788288797babd5ac42f
Pierre Rouanet <pierre.rouanet@gmail.com>
2014-07-02 12:06:29 +0200

    Correct a bug in from_vrep.

--- pypot/vrep/__init__.py
+++ pypot/vrep/__init__.py
@@ -7,8 +7,8 @@ from ..robot import Robot
 from ..robot.config import motor_from_confignode
 
 
-def from_vrep(config, vrep_port, vrep_host):
+def from_vrep(config, vrep_host, vrep_port):
     motors = [motor_from_confignode(config, name) for name in config['motors'].keys()]
-    controller = VrepController(vrep_port, vrep_host, motors)
+    controller = VrepController(vrep_host, vrep_port, motors)
 
     return Robot(motor_controllers=[controller])
