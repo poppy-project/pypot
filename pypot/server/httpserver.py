f3c98a2e79f0bd07846517c354b99a5d851dc1d0
Pierre Rouanet <pierre.rouanet@gmail.com>
2014-12-18 15:38:49 +0100

    Makes the code simpler.

--- pypot/server/httpserver.py
+++ pypot/server/httpserver.py
@@ -32,37 +32,48 @@ class HTTPRobotServer(AbstractServer):
         jd = lambda s: json.dumps(s, cls=MyJSONEncoder)
         self.app.install(bottle.JSONPlugin(json_dumps=jd))
 
+        rr = self.restfull_robot
+
         # Motors route
 
         @self.app.get('/motor/list.json')
         @self.app.get('/motor/<alias>/list.json')
         def get_motor_list(alias='motors'):
             return {
-                alias: self.restfull_robot.get_motors_list(alias)
+                alias: rr.get_motors_list(alias)
+            }
+
+        @self.app.get('/sensor/list.json')
+        def get_sensor_list():
+            return {
+                'sensors': rr.get_sensors_list()
             }
 
         @self.app.get('/motor/alias/list.json')
         def get_motor_alias():
             return {
-                'alias': self.restfull_robot.get_motors_alias()
+                'alias': rr.get_motors_alias()
             }
 
         @self.app.get('/motor/<motor_name>/register/list.json')
+        @self.app.get('/sensor/<motor_name>/register/list.json')
         def get_motor_registers(motor_name):
             return {
-                'registers': self.restfull_robot.get_registers_list(motor_name)
+                'registers': rr.get_motor_registers_list(motor_name)
             }
 
         @self.app.get('/motor/<motor_name>/register/<register_name>')
+        @self.app.get('/sensor/<motor_name>/register/<register_name>')
         def get_register_value(motor_name, register_name):
             return {
-                register_name: self.restfull_robot.get_register_value(motor_name, register_name)
+                register_name: rr.get_motor_register_value(motor_name, register_name)
             }
 
         @self.app.post('/motor/<motor_name>/register/<register_name>/value.json')
+        @self.app.post('/sensor/<motor_name>/register/<register_name>/value.json')
         def set_register_value(motor_name, register_name):
-            self.restfull_robot.set_register_value(motor_name, register_name,
-                                                   bottle.request.json)
+            rr.set_motor_register_value(motor_name, register_name,
+                                        bottle.request.json)
             return {}
 
         # Sensors route
@@ -71,59 +82,59 @@ class HTTPRobotServer(AbstractServer):
         @self.app.get('/primitive/list.json')
         def get_primitives_list(self):
             return {
-                'primitives': self.restfull_robot.get_primitives_list()
+                'primitives': rr.get_primitives_list()
             }
 
         @self.app.get('/primitive/running/list.json')
         def get_running_primitives_list(self):
             return {
-                'running_primitives': self.restfull_robot.get_running_primitives_list()
+                'running_primitives': rr.get_running_primitives_list()
             }
 
         @self.app.get('/primitive/<prim>/start.json')
         def start_primitive(self, prim):
-            self.restfull_robot.start_primitive(prim)
+            rr.start_primitive(prim)
 
         @self.app.get('/primitive/<prim>/stop.json')
         def stop_primitive(self, prim):
-            self.restfull_robot.stop_primitive(prim)
+            rr.stop_primitive(prim)
 
         @self.app.get('/primitive/<prim>/pause.json')
         def pause_primitive(self, prim):
-            self.restfull_robot.pause_primitive(prim)
+            rr.pause_primitive(prim)
 
         @self.app.get('/primitive/<prim>/resume.json')
         def resume_primitive(self, prim):
-            self.restfull_robot.resume_primitive(prim)
+            rr.resume_primitive(prim)
 
         @self.app.get('/primitive/<prim>/property/list.json')
         def get_primitive_properties_list(self, prim):
             return {
-                'property': self.restfull_robot.get_primitive_properties_list(prim)
+                'property': rr.get_primitive_properties_list(prim)
             }
 
         @self.app.get('/primitive/<prim>/property/<prop>')
         def get_primitive_property(self, prim, prop):
-            res = self.restfull_robot.get_primitive_property(prim, prop)
+            res = rr.get_primitive_property(prim, prop)
             return {
                 '{}.{}'.format(prim, prop): res
             }
 
         @self.app.post('/primitive/<prim>/property/<prop>/value.json')
         def set_primitive_property(self, prim, prop):
-            self.restfull_robot.set_primitive_property(prim, prop,
-                                                       bottle.request.json)
+            rr.set_primitive_property(prim, prop,
+                                      bottle.request.json)
 
         @self.app.get('/primitive/<prim>/method/list.json')
         def get_primitive_methods_list(self, prim):
             return {
-                'methods': self.restfull_robot.get_primitive_methods_list(self, prim)
+                'methods': rr.get_primitive_methods_list(self, prim)
             }
 
         @self.app.post('/primitive/<prim>/method/<meth>/args.json')
         def call_primitive_method(self, prim, meth):
-            res = self.restfull_robot.call_primitive_method(prim, meth,
-                                                            bottle.request.json)
+            res = rr.call_primitive_method(prim, meth,
+                                           bottle.request.json)
             return {
                 '{}:{}'.format(prim, meth): res
             }
