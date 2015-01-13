0d998bc92ef4b3d54b9c2be274c90e3a64206f79
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-01-13 19:35:28 +0100

    Use a decorator for creating a snap compatible http answer.

--- pypot/server/snap.py
+++ pypot/server/snap.py
@@ -3,6 +3,21 @@ import bottle
 from .server import AbstractServer
 
 
+def make_snap_compatible_response(f):
+    def wrapped_f(*args, **kwargs):
+        msg = f(*args, **kwargs)
+
+        r = bottle.response
+        r.status = '200 OK'
+        r.set_header('Content-Type', 'text/html')
+        r.set_header('charset', 'ISO-8859-1')
+        r.set_header('Content-Length', len(msg))
+        r.set_header('Access-Control-Allow-Origin', '*')
+
+        return msg
+
+    return wrapped_f
+
 
 class SnapRobotServer(AbstractServer):
     def __init__(self, robot, host, port):
@@ -12,41 +27,23 @@ class SnapRobotServer(AbstractServer):
 
         rr = self.restfull_robot
 
-        @self.app.get('/motors')
-        def get_motors():
-            print '/'.join(rr.get_motors_list())
-            return '/'.join(rr.get_motors_list())
+        @self.app.get('/motors/<alias>')
+        @make_snap_compatible_response
+        def get_motors(alias):
+            return '/'.join(rr.get_motors_list(alias))
 
         @self.app.get('/motor/<motor>/get/<register>')
+        @make_snap_compatible_response
         def get_motor_register(motor, register):
-            s = str(rr.get_motor_register_value(motor, register))
+            return str(rr.get_motor_register_value(motor, register))
 
-            r = bottle.response
-            r.status = '200 OK'
-            r.set_header('Content-Type', 'text/html')
-            r.set_header('charset', 'ISO-8859-1')
-            r.set_header('Content-Length', len(s))
-            r.set_header('Access-Control-Allow-Origin', '*')
-
-
-            return s
 
         @self.app.get('/motor/<motor>/set/<register>/<value>')
+        @make_snap_compatible_response
         def set_pos(motor, register, value):
             rr.set_motor_register_value(motor, register, float(value))
+            return 'done!'
 
-            s = str('done!')
-
-            r = bottle.response
-            r.status = '200 OK'
-            r.set_header('Content-Type', 'text/html')
-            r.set_header('charset', 'ISO-8859-1')
-            r.set_header('Content-Length', len(s))
-            r.set_header('Access-Control-Allow-Origin', '*')
-
-            # t.append(time.time())
-
-            return s
         print 'Snap Server should now be ready!'
 
     def run(self):
