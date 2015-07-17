82c5c1ce0b159f4f25584e296c4222b66d07148d
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-07-17 18:20:02 +0200

    Quite massive re-writing of dxl controller to make them either synchronous or asynchronous.

--- pypot/utils/__init__.py
+++ pypot/utils/__init__.py
@@ -1,8 +1,9 @@
 from collections import namedtuple
 from operator import attrgetter
+from threading import Event
 
 from .stoppablethread import StoppableThread, StoppableLoopThread
-
+from .pypot_time import time
 
 Point2D = namedtuple('Point2D', ('x', 'y'))
 Point3D = namedtuple('Point3D', ('x', 'y', 'z'))
@@ -27,3 +28,30 @@ def attrsetter(item):
         setattr(resolve_attr(obj, var_path), var_name, value)
 
     return g
+
+
+class SyncEvent(object):
+    def __init__(self, period=.1):
+        self._event = Event()
+        self._needed = False
+
+        self._last_sync = 0.
+        self.period = period
+
+    def request(self):
+        self._needed = True
+        self._event.wait()
+        self._event.clear()
+
+    def done(self):
+        self._event.set()
+        self._last_sync = time()
+        self._needed = False
+
+    @property
+    def is_recent(self):
+        return (time() - self._last_sync) < self.period
+
+    @property
+    def needed(self):
+        return self._needed and not self.is_recent
