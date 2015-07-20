ee2f969bbe6f21a4e6a5bd85890bc8326d74909d
Théo Segonds <theo.segonds@inria.fr>
2015-07-20 15:47:40 +0200

    install test version

--- ci/install.sh
+++ ci/install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 echo "Running install.sh on $TRAVIS_OS_NAME"
 
+python -c "import sys; print(sys.version_info);print(sys.version_info >= (3,))"
+
 # Verify python version
 python --version
 
 cd ${SETUP_DIR}
-python setup.py install
+python setup.py develop
 
-
+set -x
+cat build/lib/pypot/vrep/__init__.py |grep str
+cat build/lib/pypot/vrep/__init__.py |grep basestring
+set +x
