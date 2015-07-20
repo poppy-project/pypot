a066182c92970875a1f00baa4eb0b9b0ead46030
Théo Segonds <theo.segonds@inria.fr>
2015-07-20 15:47:40 +0200

    Fix test path that cause failure of python 3.4

--- ci/install.sh
+++ ci/install.sh
@@ -4,7 +4,6 @@ echo "Running install.sh on $TRAVIS_OS_NAME"
 # Verify python version
 python --version
 
-cd ${SETUP_DIR}
-python setup.py install
-
-
+pushd $SETUP_DIR
+    python setup.py install
+popd
