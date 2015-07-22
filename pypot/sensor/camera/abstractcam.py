9692b689c4254de78c4393b468c78464d438ff46
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-07-22 16:40:52 +0200

    Update camera sensor so the grab and processing run inside a thread.

--- pypot/sensor/camera/abstractcam.py
+++ pypot/sensor/camera/abstractcam.py
@@ -1,3 +1,7 @@
+import time
+
+from threading import Thread
+
 from ...robot.sensor import Sensor
 
 
@@ -8,11 +12,15 @@ class AbstractCamera(Sensor):
         Sensor.__init__(self, name)
 
         self._res, self._fps = resolution, fps
+        self._last_frame = self._grab_and_process()
+
+        self._processing = Thread(target=self._process_loop)
+        self._processing.daemon = True
+        self._processing.start()
 
     @property
     def frame(self):
-        frame = self.grab()
-        return self.post_processing(frame)
+        return self._last_frame
 
     def post_processing(self, image):
         return image
@@ -20,9 +28,19 @@ class AbstractCamera(Sensor):
     def grab(self):
         raise NotImplementedError
 
+    def _grab_and_process(self):
+        return self.post_processing(self.grab())
+
+    def _process_loop(self):
+        period = 1.0 / self.fps
+
+        while True:
+            self._last_frame = self._grab_and_process()
+            time.sleep(period)
+
     @property
     def resolution(self):
-        return self._res
+        return list(reversed(self.frame.shape[:2]))
 
     @property
     def fps(self):
