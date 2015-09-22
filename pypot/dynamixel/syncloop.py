dce075875457c656af05aeb70226822f516ece5f
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-09-22 11:53:17 +0200

    Allows for faster led sync.

--- pypot/dynamixel/syncloop.py
+++ pypot/dynamixel/syncloop.py
@@ -96,7 +96,7 @@ class LightDxlController(MetaDxlController):
 
         led_motors = [m for m in motors if m.model.startswith('XL-320')]
         if led_motors:
-            controllers.append(DxlController(io, led_motors, 1., False,
+            controllers.append(DxlController(io, led_motors, 5., False,
                                              'set', 'LED_color', 'led'))
 
         MetaDxlController.__init__(self, io, motors, controllers)
