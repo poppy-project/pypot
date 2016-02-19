39c599aa395fc3fb687746c8adaaebce1aa3be06
Théo Segonds <theo.segonds@inria.fr>
2016-02-19 03:01:09 +0100

    Update find_local_ip to make it more generic

--- pypot/server/snap.py
+++ pypot/server/snap.py
@@ -24,15 +24,18 @@ def get_snap_user_projects_directory():
 def find_local_ip():
     # see here: http://stackoverflow.com/questions/166506/
     try:
-        return [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close())
-                for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+        return [l for l in ([ip for ip in
+               socket.gethostbyname_ex(socket.gethostname())[2]
+               if not ip.startswith('127.')][:1], [[(s.connect(('8.8.8.8',
+               53)), s.getsockname()[0], s.close()) for s in
+               [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]])
+               if l][0][0]
+
     except IOError, e:
-        # an IOError exception occurred (socket.error is a subclass)
-        if e.errno == 101:
-            # now we had the error code 101, network unreachable
+        # network unreachable
+        if e.errno == errno.ENETUNREACH:
             return '127.0.0.1'
         else:
-            # other exceptions we reraise again
             raise IOError(e)
 
 
