a4860525b3125a14f7a3ec33eeede5c26a51009c
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-03-07 14:25:18 +0100

    Typos in travis.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -29,6 +29,8 @@ else
             # Download a PoppyHumanoid ipython notebook
             curl -l -o nb.py https://gist.githubusercontent.com/pierre-rouanet/e6b89340a7ec781c5355/raw/64bb25ba01e077160e7b04e485d78164ac53fdf6/nb.py
             python nb.py
+
+            pip uninstall -y poppy-creature poppy-humanoid
         fi
 
         # Old test of running VREP localy (network trouble)
@@ -45,7 +47,6 @@ else
 
     pushd tests
         pip install coverage requests
-        pip uninstall -y poppy-creature
         pip install --pre poppy-creature poppy-ergo-jr
         python -m unittest discover
     popd
