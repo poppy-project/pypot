3dabc32d76bed9fd54bb9516381d06c8597c0097
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-03-31 19:14:13 +0200

    Make PowerContactor more pythonic (I may have broken some stuff tough...)

--- pypot/sensor/power_control/power.py
+++ pypot/sensor/power_control/power.py
@@ -4,46 +4,42 @@ from ...robot.sensor import Sensor
 
 
 class PowerContactor(Sensor):
-    """this class give access to power contactor"""
-    registers = Sensor.registers + ['status_list']
+    """ this class gives access to power contactors """
+    registers = Sensor.registers + ['status']
 
-    def __init__(self, name, device_list):
+    def __init__(self, name, devices):
+        """
+            devices must be formated such as:
+            devices = {
+                'name1': [pin_number, normal_mode],
+                'name2': [pin_number, normal_mode],
+                ...
+                # where normal_mode must be in ('normal_open', 'normal_close')
+            }
+        """
         Sensor.__init__(self, name)
-        self.normal = {'normalOpen': 0,
-                       'normalClose': 1}
-        # devicelist is formated like
-        # 'device name': [pinNumber, normal ('normalOpen' or 'normalClose')]
-        self._device_list = device_list
-        self._status_list = {}
+
+        self._devices = devices
+        self._status = {}
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
-        for item in self._device_list:
-            GPIO.setup(self._device_list[item][0], GPIO.OUT)
-            GPIO.output(self._device_list[item][0], GPIO.HIGH)
-            if self._device_list[item][1] == 'normalOpen':
-                self._status_list[item] = 'disable'
-            else:
-                self._status_list[item] = 'enable'
+        for name, (pin, normal) in self._devices.items():
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.HIGH)
+            self._status[name] = 'disable' if normal == 'normal_open' else 'enable'
 
     @property
-    def status_list(self):
-        return self._status_list
+    def status(self):
+        return self._status
 
     def enable(self, device):
-        if self._device_list[device][1] == 'normalClose':
-            GPIO.output(self._device_list[device][0],
-                        GPIO.HIGH)
-        else:
-            GPIO.output(self._device_list[device][0],
-                        GPIO.LOW)
-        self._status_list[device] = 'enable'
+        pin, normal = self._devices[device]
+        GPIO.output(pin, GPIO.HIGH if normal == 'normal_close' else GPIO.LOW)
+        self._status[device] = 'enable'
 
     def disable(self, device):
-        if self._device_list[device][1] == 'normalClose':
-            GPIO.output(self._device_list[device][0],
-                        GPIO.LOW)
-        else:
-            GPIO.output(self._device_list[device][0],
-                        GPIO.HIGH)
-        self._status_list[device] = 'disable'
+        pin, normal = self._devices[device]
+        GPIO.output(pin, GPIO.LOW if normal == 'normal_close' else GPIO.HIGH)
+        self._status[device] = 'disable'
