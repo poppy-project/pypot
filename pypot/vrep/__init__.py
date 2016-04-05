0f57e771ee93156640e82b2fc25cda57852a6b27
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-04-05 11:33:29 +0200

    Give it more time to startup.

--- pypot/vrep/__init__.py
+++ pypot/vrep/__init__.py
@@ -25,10 +25,13 @@ class vrep_time():
     def __init__(self, vrep_io):
         self.io = vrep_io
 
-    def get_time(self):
+    def get_time(self, trial=0):
         t = self.io.get_simulation_current_time()
 
         if t == 0:
+            return get_time(trial + 1)
+
+        if trial > 10:
             raise EnvironmentError('Could not get current simulation time. Make sure the V-REP simulation is running. And that you have added the "time" child script to your scene.')
 
         return t
