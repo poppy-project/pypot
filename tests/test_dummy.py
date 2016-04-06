0e229133487a92faa1d436dcabb5be490be9a628
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-04-06 16:10:34 +0200

    Add a unittest for issue #166

---
+++ tests/test_dummy.py
@@ -0,0 +1,24 @@
+import time
+import unittest
+
+from poppy.creatures import PoppyErgoJr
+
+
+class TestIK(unittest.TestCase):
+    def setUp(self):
+        self.jr = PoppyErgoJr(simulator='poppy-simu')
+
+    def test_dummy_controller(self):
+        for m in self.jr.motors:
+            m.goal_position = 25
+
+        # Make sure it was synced
+        self.jr._controllers[0]._updated.clear()
+        self.jr._controllers[0]._updated.wait()
+
+        for m in self.jr.motors:
+            self.assertEqual(m.goal_position, m.present_position)
+
+
+if __name__ == '__main__':
+    unittest.main()
