e115f0de871319316f73b9a8d57f2a13f15db690
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-05-24 12:02:03 +0200

    Make pep8 happier.

---
+++ pypot/sensor/range/__init__.py
@@ -0,0 +1,1 @@
+from .sonar import Sonar
--- pypot/sensor/range/sonar.py
+++ pypot/sensor/range/sonar.py
@@ -1,46 +1,46 @@
 from __future__ import print_function
+
 import time
-import numpy as np
 
 from ...robot.sensor import Sensor
-from ...utils import StoppableLoopThread
 
 try:
     import smbus
 except ImportError:
-    print('''You need to install smbus:
+    print('''You need to install smbus first.
           sudo apt-get install build-essential libi2c-dev i2c-tools python-dev libffi-dev
           pip install smbus-cffi''')
 
 
 class SonarSensor(Sensor):
-	""" Give acces to ultrasonic I2C modules SRF-02 in a *pypot way* """
+    """ Give acces to ultrasonic I2C modules SRF-02 in a *pypot way* """
 
-	def __init__(self,)
-		pass
+    def __init__(self):
+        pass
 
 
-class Sonar():
+class Sonar(object):
     """ Give acces to ultrasonic I2C modules SRF-02 connected with I2C pin of your board.
-        To have more informations, go to http://www.robot-electronics.co.uk/htm/srf02techI2C.htm
+        To get more information, go to http://www.robot-electronics.co.uk/htm/srf02techI2C.htm
 
         Example:
 
         > i2c = smbus.SMBus(1)
-        > sonar = Sonar(i2c, adresses=[0x70,0x71,0x72])
+        > sonar = Sonar(i2c, addresses=[0x70, 0x71, 0x72])
         >
     """
 
-    def __init__(self, i2c, addresses=None):
-        self.i2c = i2c
+    def __init__(self, i2c, addresses=[0x70]):
+        """ 0x70 is the default address for the SRF-02 I2C module. """
 
-        # 0x70 is the default adress of SRF-02 I2C module
-        self.addresses = addresses if addresses is not None else [0x70]
-        self.datas = None
+        self.i2c = i2c
+        self.addresses = addresses
+
+        self.data = None
 
         self._raw_data_queues = []
         for _ in self.addresses:
-            self._raw_data_queues.append(5 * [0, ])
+            self._raw_data_queues.append(5 * [0])
 
         self.results_type = {'inches': 0x50,
                              'centimeters': 0x51,
@@ -51,8 +51,8 @@ class Sonar():
     def update(self):
         self.ping()
         time.sleep(0.065)
-        self.datas = self._filter(self.read())
-        return self.datas
+        self.data = self._filter(self.read())
+        return self.data
 
     def ping(self):
         for addr in self.addresses:
@@ -61,46 +61,50 @@ class Sonar():
     def read(self, reg=2):
         return [self._read(addr, reg) for addr in self.addresses]
 
-    def _filter(self, datas):
-        """Apply a filter to reduce noisy datas
-           return the median value on a heap of 5 raw datas
+    def _filter(self, data):
+        """ Apply a filter to reduce noisy data.
+
+           Return the median value on a heap of 5 raw data.
+
         """
-        filtered_datas = []
-        for queue, data in zip(self._raw_data_queues, datas):
+        filtered_data = []
+        for queue, data in zip(self._raw_data_queues, data):
             queue.pop(0)
             queue.append(data)
-            filtered_datas.append(sorted(queue)[2])
-        return filtered_datas
+            filtered_data.append(sorted(queue)[2])
+
+        return filtered_data
 
     def _ping(self, address, data=None):
         d = data if data is not None else self.results_type['centimeters']
-        while 1:
+
+        while True:
             try:
                 self.i2c.write_byte_data(address, 0, d)
                 break
             except IOError:
                 time.sleep(0.005)
                 self.__errors += 1
-                continue
 
     def _read(self, address, reg=2):
-        while 1:
+        while True:
             try:
                 return int(self.i2c.read_word_data(address, reg)) / 256
             except IOError:
                 time.sleep(0.005)
                 self.__errors += 1
-                continue
 
- if __name__ == '__main__':
-	from pylab import plot
-	i2c = smbus.SMBus(1)
-	sonar = Sonar(i2c)
 
-	d = []
-	t = [time.time()]
-	for _ in range(1000):
-	    sonar.update()
-	    d.append(sonar.datas[0])
-	    t.append(time.time() - t[0])
-	plot(t[1:], d)
+if __name__ == '__main__':
+    from pylab import plot
+
+    i2c = smbus.SMBus(1)
+    sonar = Sonar(i2c)
+
+    d = []
+    t = [time.time()]
+    for _ in range(1000):
+        sonar.update()
+        d.append(sonar.data[0])
+        t.append(time.time() - t[0])
+    plot(t[1:], d)
