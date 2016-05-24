d72b238659623011904d374d5ffeed7f012f0e6d
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-05-24 15:48:05 +0200

    Make the import homogeneous with the rest of pypot sensors.

--- pypot/sensor/depth/__init__.py
+++ pypot/sensor/depth/__init__.py
@@ -2,6 +2,4 @@ try:
     from .sonar import SonarSensor
 
 except ImportError:
-    print('''You need to install smbus first.
-          sudo apt-get install build-essential libi2c-dev i2c-tools python-dev libffi-dev
-          pip install smbus-cffi''')
+    pass
