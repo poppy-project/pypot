4fc148c1d13c47eaf39f6e4fb0415559accea960
Théo Segonds <theo.segonds@inria.fr>
2016-06-15 19:59:58 +0200

    Add a VCC pin

--- pypot/sensor/contact/contact.py
+++ pypot/sensor/contact/contact.py
@@ -1,20 +1,20 @@
-import RPi.GPIO as GPIO
-
-from ...robot.sensor import Sensor
-
-
 class ContactSensor(Sensor):
-    """ This class gives access to a contact sensor. """
+    """ Gives access to a micro switch sensor. """
     registers = Sensor.registers + ['contact']
 
-    def __init__(self, name, gpio_number):
+    def __init__(self, name, gpio_data, gpio_vcc=None):
         Sensor.__init__(self, name)
 
-        self._pin = gpio_number
-
+        self._pin = gpio_data
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self._pin, GPIO.IN)
+        
+        if gpio_vcc is not None:
+            self._vcc = gpio_vcc
+            GPIO.setup(self._vcc, GPIO.OUT)     
+            GPIO.output(self._vcc, GPIO.HIGH)
+        
 
     @property
     def contact(self):
