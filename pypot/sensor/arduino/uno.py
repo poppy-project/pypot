d86894a04e082df59f25bd0f15b2994830ba61ab
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-08-03 09:26:44 +0200

    Pep8ification.

--- pypot/sensor/arduino/uno.py
+++ pypot/sensor/arduino/uno.py
@@ -6,19 +6,18 @@ from ...utils import StoppableLoopThread
 
 
 class ArduinoSensor(Sensor):
-    """ Give acces to arduino sensor
-		
-		Be careful to set the sync_freq of your controller faster than the data comes from your arduino
-		If not, you won't be able to retrieve the more recent data
-        
+    """ Give acces to arduino sensor.
+
+        Be careful to set the sync_freq of your controller faster than the data comes from your arduino.
+        If not, you won't be able to retrieve the more recent data.
+
     """
-    
     def __init__(self, name, port, baud, sync_freq=50.0):
         Sensor.__init__(self, name)
         self.port = port
         self.baud = baud
         self._controller = StoppableLoopThread(sync_freq, update=self.update)
-        
+
     def start(self):
         self._ser = serial.Serial(self.port, self.baud)
         self._controller.start()
@@ -30,7 +29,7 @@ class ArduinoSensor(Sensor):
     def update(self):
         while self._ser.inWaiting() > 0:
             line = self._ser.readline()
-        try: 
+        try:
             self.sensor_dict = json.loads(line)
         except:
-            pass    
+            pass
