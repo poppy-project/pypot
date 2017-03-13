e68c89c76834b5b1027259313e78615cdb39fbc0
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-03-13 11:14:55 +0100

    FIx ci path for miniconda.

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -33,7 +33,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
     # Use miniconda python (provide binaries for scipy and numpy on Linux)
     if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-        curl -L -o miniconda.sh http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
+        curl -L -o miniconda.sh http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
     else
         curl -L -o miniconda.sh http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
     fi
@@ -48,7 +48,7 @@ elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
     # Use miniconda python (provide binaries for scipy and numpy on Linux)
     if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-        curl -L -o miniconda.sh http://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh
+        curl -L -o miniconda.sh http://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
     else
         curl -L -o miniconda.sh http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
     fi
