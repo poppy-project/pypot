a7399a52ad0f4674813bb43f82de03b02315cacd
Julien JEHL <julien.jehl@free.fr>
2017-04-07 14:22:02 +0200

    Update roboticia creatures (#243)

--- pypot/creatures/__init__.py
+++ pypot/creatures/__init__.py
@@ -7,8 +7,11 @@ module = sys.modules[__name__]
 
 installed_poppy_creatures = {}
 # Feel free to make a pull request to add your own creature here
-existing_creatures = ['poppy-humanoid', 'poppy-torso', 'poppy-ergo-jr', 'poppy-ergo-starter', 'poppy-6dof-right-arm', 'poppy-dragster-mini', 'poppy-ergo', 'roboticia-quattro', 'roboticia-starter', 'doggy']
-
+existing_creatures = ['poppy-humanoid', 'poppy-torso', 'poppy-ergo-jr',
+                      'poppy-ergo-starter', 'poppy-6dof-right-arm',
+                      'poppy-dragster-mini', 'poppy-ergo', 'roboticia-quattro',
+                      'roboticia-first', 'roboticia-uno', 'roboticia-drive',
+                      'doggy']
 
 for creature in existing_creatures:
     package = creature.replace('-', '_')
