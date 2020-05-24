2bcf7cfbef76806bb8b5054e6203c15e261511ef
Yoan Mollard <ymollard@users.noreply.github.com>
2020-05-24 16:31:51 +0200

    Bugfix fixed ikpy version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = ['numpy',
                     'pyserial>2.6',
                     'tornado',
                     'scipy',
-                    'ikpy',
+                    'ikpy==2.3.3',
                     'bottle',
                     'requests',
                     'poppy-creature>=2'  # Kept to avoid breaking old imports
