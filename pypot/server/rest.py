3ac2cb1f5bd60fa0e44200c8536e57a2dd3b1a79
Tibo42 <thibault.desprez@gmail.com>
2020-05-26 12:35:47 +0200

    fix path for move recorder

--- pypot/server/rest.py
+++ pypot/server/rest.py
@@ -24,7 +24,6 @@ class RESTRobot(object):
     def __init__(self, robot):
         self.robot = robot
         self.moves_path=""
-        #os.makedirs(self.moves_path, exist_ok=True)
 
     # Access motor related values
 
@@ -150,6 +149,7 @@ class RESTRobot(object):
         """Allow more easily than stop_primitive() to save in a filename the recorded move"""
         recorder = getattr(self.robot, '_{}_recorder'.format(move_name))
         recorder.stop()
+        #os.makedirs(self.moves_path, exist_ok=True)
         with open('{}.record'.format(self.moves_path+move_name), 'w') as f:
             recorder.move.save(f)
 
@@ -175,6 +175,7 @@ class RESTRobot(object):
             pass
 
         # if not running, override the play primitive
+        #os.makedirs(self.moves_path, exist_ok=True)
         with open('{}.record'.format(self.moves_path+move_name)) as f:
             loaded_move = Move.load(f)
         player = MovePlayer(self.robot, loaded_move, play_speed=speed, backwards=backwards)
@@ -185,8 +186,10 @@ class RESTRobot(object):
 
     def get_available_record_list(self):
         """Get list of json recorded movement files"""
+        #os.makedirs(self.moves_path, exist_ok=True)
         return [f.split('.record')[0] for f in os.listdir('./'+self.moves_path) if f.endswith('.record')]
 
     def remove_move_record(self, move_name):
         """Remove the json recorded movement file"""
+        #os.makedirs(self.moves_path, exist_ok=True)
         return os.remove('{}.record'.format(self.moves_path+move_name))
